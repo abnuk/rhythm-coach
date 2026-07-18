347fda5c54c833e217175d40ecdad5f9bdd36838
Marcin Jabłonowski <marcin.jablonowski@pragmile.com>
2026-07-18 12:09:57 +0300

    Bump version to 1.1.1

--- Scripts/make-app.sh
+++ Scripts/make-app.sh
@@ -39,8 +39,8 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
     <key>CFBundleIconFile</key><string>AppIcon</string>
     <key>CFBundleIconName</key><string>AppIcon</string>
     <key>CFBundlePackageType</key><string>APPL</string>
-    <key>CFBundleShortVersionString</key><string>1.1.0</string>
-    <key>CFBundleVersion</key><string>2</string>
+    <key>CFBundleShortVersionString</key><string>1.1.1</string>
+    <key>CFBundleVersion</key><string>3</string>
     <key>LSMinimumSystemVersion</key><string>15.0</string>
     <key>NSHighResolutionCapable</key><true/>
     <key>NSMicrophoneUsageDescription</key>
--- Scripts/make-release.sh
+++ Scripts/make-release.sh
@@ -5,7 +5,7 @@
 set -euo pipefail
 
 cd "$(dirname "$0")/.."
-VERSION="${1:-1.1.0}"
+VERSION="${1:-1.1.1}"
 
 ./Scripts/make-app.sh release --universal
 
